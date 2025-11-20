4062ea229fa10ec35a5e760117cf3cf1fbdf744e
wemsx <3551697397@qq.com>
2025-11-21 00:39:36 +0800

    update new slidev

---
+++ assets/slidev/default-DkUK62Lq.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-Beuf04Y5.js";import{_ as r}from"../index-C8-vy_on.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
